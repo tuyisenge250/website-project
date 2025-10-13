b54490a9015498590e2ba74a08cfb520e6f05d4a
tuyisenge250 <benjaminwell250@gmail.com>
2025-10-13 19:24:54 +0200

    tailwind updated to new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
