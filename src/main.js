d2a5133d349a25c9186134daf68f235dde20851f
iToussaint <iradukundatoussaint01@gmail.com>
2025-10-16 19:13:46 +0200

    ft/Add interactivity in the header section

--- src/main.js
+++ src/main.js
@@ -1,24 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+function header() {
+  return {
+    navItems: document.getElementById("nav-items"),
+    hamburgerMenu: document.getElementById("hamburger-menu"),
+  };
+}
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+setTimeout(() => {
+  header().hamburgerMenu.addEventListener("click", function (event) {
+    [...this.children].forEach(element => {
+      element.classList.toggle("hidden")
+    });
+    this.nextElementSibling.classList.toggle("scale-y-100");
+  });
+  console.log(header());
+}, 1000);
