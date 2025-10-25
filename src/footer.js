652b48ad499002ad42617770914571cd81758b41
tuyisenge250 <benjaminwell250@gmail.com>
2025-10-25 09:43:01 +0200

    create responsive footer with dynamic content sections

--- src/footer.js
+++ src/footer.js
@@ -4,6 +4,21 @@ function footerLocation(){
     if (footerLocationElement) {
         footerLocationElement.innerHTML = text.map(line => `<p>${line}</p>`).join('');
     }
+    const footerContactElement = document.querySelector('#footer-contact');
+    const textContact = ["hallo@agentur-baumeister.com", "+49 (0)30 - 490 827 87"]
+    if (footerContactElement) {
+        footerContactElement.innerHTML = textContact.map(line => `<p>${line}</p>`).join('');
+    }
+    const footerSocialElement = document.querySelector('#footer-social-media');
+    const textSocial = ["LinkedIn", "Instagram"]
+    if (footerSocialElement) {
+        footerSocialElement.innerHTML = textSocial.map(line => `<p>${line}</p>`).join('');
+    }
+    const textLegal = ["Datenschutz", "Impressum"]
+ 
+    footerSocialElement.innerHTML += `<div class="flex justify-between w-[50%] md:w-[40%] lg:flex-col gap-0 lg:w-[15%] lg:justify-start" id="footer-legal">${textLegal.map(line => `<p>${line}</p>`).join('')}</div>`
+
+
 }
 
 setTimeout(() => {
