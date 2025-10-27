c0b99cafa0ff272831c474bcf571f5fb9e7ef372
iToussaint <iradukundatoussaint01@gmail.com>
2025-10-27 17:30:28 +0200

    ft/header scripts file

---
+++ src/header.js
@@ -0,0 +1,63 @@
+function header() {
+  return {
+    navItems: document.getElementById("nav-items"),
+    hamburgerMenu: document.getElementById("hamburger-menu"),
+  };
+}
+
+setTimeout(() => {
+  header().hamburgerMenu.addEventListener("click", function (event) {
+    [...this.children].forEach(element => {
+      element.classList.toggle("hidden")
+    });
+    this.nextElementSibling.classList.toggle("hidden");
+    this.nextElementSibling.classList.toggle("flex");
+  });
+  console.log(header());
+}, 1000);
+function header() {
+  return {
+    navItems: document.getElementById("nav-items"),
+    hamburgerMenu: document.getElementById("hamburger-menu"),
+  };
+}
+
+setTimeout(() => {
+  header().hamburgerMenu.addEventListener("click", function (event) {
+    [...this.children].filter((_, index) => index !== 0).forEach((element) => {
+      element.classList.toggle("hidden");
+    });
+
+
+    this.nextElementSibling.classList.toggle("hidden");
+    this.nextElementSibling.classList.toggle("flex");
+  });
+}, 1000);
+
+function createNavList(array) {
+  return array.map((navItem) => {
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+    link.setAttribute("href", `#${navItem.toLowerCase()}`)
+    link.textContent = navItem;
+    listItem.append(link);
+    return listItem;
+  });
+}
+
+function createLanguageList(array) {
+  return array.map((language) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = language;
+    return listItem;
+  });
+}
+
+setTimeout(() => {
+  const languagesList = document.getElementById("languages");
+  const listItems = document.getElementById("nav-items");
+  languagesList.append(...createLanguageList(["DE", "EN"]));
+  listItems.append(
+    ...createNavList(["About", "Services", "References", "Contact"])
+  );
+}, 1000);
