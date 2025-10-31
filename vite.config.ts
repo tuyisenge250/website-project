ff9ea61341cbc9307844beb67819b1bb8c989de9
tuyisenge250 <benjaminwell250@gmail.com>
2025-10-31 15:19:11 +0200

    test deployed version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: './', // ✅ Important for Netlify and static hosts
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    outDir: 'dist', // ✅ Ensure all final files go here
+  },
 })
